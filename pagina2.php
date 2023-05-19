9baca73358b08051f1bdcd5eb0c5768fcc492ebc
BiaMayumi <109619902+BiaMayumi@users.noreply.github.com>
2023-05-19 16:40:04 -0300

    commit da Bia

--- pagina2.php
+++ pagina2.php
@@ -3,58 +3,137 @@
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Page Title</title>
+    <title>Reconhecedor de Signo</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <script src='main.js'></script>
 </head>
 <body>
-    
+    <h1>HORÓSCOPO</h1>
+    <form action="pagina2.php" method="get">
+        <br><label for="lblData">Escolha uma data: </label>
+        <input type="date" name="data">
+        <input type="submit">
+    </form>
+
     <?php
-        $data = date_parse($_GET["data"]);
-        
-        switch($data["month"]){
-            case 1:
-            case 2:
-            case 3:
-                if($data["month"] == 3 and $data["day"] >= 20){
-                    echo "<img src='img/outono.png'>";
-                } else{
-                    echo "<img src='img/verao.png'>";
-                }
-                break;
-            case 4:
-            case 5:
-            case 6:
-                if($data["month"] == 6 and $data["day"] >= 21){
-                    echo "<img src='img/inverno.png'>";
-                }else{
-                    echo "<img src='img/outono.png'>";
-                }
-                break;
-            case 7:
-            case 8:
-            case 9:
-                if($data["month"] == 9 and $data["day"] >= 23){
-                    echo "<img src='img/primavera.png'>";
-                } else {
-                    echo "<img src='img/inverno.png'>";
-                }
-                break;
-            case 10:
-            case 11:
-            case 12:
-                if($data["month"] == 12 and $data["day"] >= 22){
-                    echo "<img src='img/verao.png'>";
-                } else {
-                    echo "<img src='img/primavera.png'>";
-                }
-                break;
-            default:
-                echo "<p>Mês " .$data["month"]. " incorreto!</p>";        
+        function getSigno($mes, $dia){
+            switch($mes){
+                case 1:
+                    if($dia >= 20){
+                        echo "<p>Seu signo é: Aquario </p>";
+                        echo "<img src='img/aquario.png'>";
+                    } else{
+                        echo "<p>Seu signo é: Capricornio </p>";
+                        echo "<img src='img/capricornio.png'>";
+                    }
+                    break;
+                case 2:
+                    if($dia >= 19){
+                        echo "<p>Seu signo é: Peixes </p>";
+                        echo "<img src='img/peixes.png'>";
+                    } else{
+                        echo "<p>Seu signo é: Aquario </p>";
+                        echo "<img src='img/aquario.png'>";
+                    }
+                    break;
+                case 3:
+                    if($dia >= 21){
+                        echo "<p>Seu signo é: Áries </p>";
+                        echo "<img src='img/aries.png'>";
+                    } else{
+                        echo "<p>Seu signo é: Peixes </p>";
+                        echo "<img src='img/peixes.png'>";
+                    }
+                    break;
+                case 4:
+                    if($dia >= 20){
+                        echo "<p>Seu signo é: Touro </p>";
+                        echo "<img src='img/touro.png'>";
+                    } else{
+                        echo "<p>Seu signo é: Áries </p>";
+                        echo "<img src='img/aries.png'>";
+                    }
+                    break;
+                case 5:
+                    if($dia >= 21){
+                        echo "<p>Seu signo é: Gêmeos </p>";
+                        echo "<img src='img/gemeos.png'>";
+                    } else{
+                        echo "<p>Seu signo é: Touro </p>";
+                        echo "<img src='img/touro.png'>";
+                    }
+                    break;
+                case 6:
+                    if($dia >= 21){
+                        echo "<p>Seu signo é: Câncer </p>";
+                        echo "<img src='img/cancer.png'>";
+                    } else{
+                        echo "<p>Seu signo é: Gêmeos </p>";
+                        echo "<img src='img/gemeos.png'>";
+                    }
+                    break;
+                case 7:
+                    if($dia >= 23){
+                        echo "<p>Seu signo é: Leão </p>";
+                        echo "<img src='img/leao.png'>";
+                    } else{
+                        echo "<p>Seu signo é: Câncer </p>";
+                        echo "<img src='img/cancer.png'>";
+                    }
+                    break;
+                case 8:
+                    if($dia >= 23){
+                        echo "<p>Seu signo é: Virgem </p>";
+                        echo "<img src='img/virgem.png'>";
+                    } else{
+                        echo "<p>Seu signo é: Leão </p>";
+                        echo "<img src='img/leao.png'>";
+                    }
+                    break;
+                case 9:
+                    if($dia >= 23){
+                        echo "<p>Seu signo é: Libra </p>";
+                        echo "<img src='img/libra.png'>";
+                    } else{
+                        echo "<p>Seu signo é: Virgem </p>";
+                        echo "<img src='img/virgem.png'>";
+                    }
+                    break;
+                case 10:
+                    if($dia >= 23){
+                        echo "<p>Seu signo é: Escorpião </p>";
+                        echo "<img src='img/escorpiao.png'>";
+                    } else{
+                        echo "<p>Seu signo é: Libra </p>";
+                        echo "<img src='img/libra.png'>";
+                    }
+                    break;
+                case 11:
+                    if($dia >= 22){
+                        echo "<p>Seu signo é: Sagitário </p>";
+                        echo "<img src='img/sagitario.png'>";
+                    } else{
+                        echo "<p>Seu signo é: Escorpião </p>";
+                        echo "<img src='img/escorpiao.png'>";
+                    }
+                    break;
+                case 12:
+                    if($dia >= 22){
+                        echo "<p>Seu signo é: Capricornio </p>";
+                        echo "<img src='img/capricornio.png'>";
+                    } else{
+                        echo "<p>Seu signo é: Sagitário </p>";
+                       echo "<img src='img/sagitario.png'>";
+                    }
+                    break;
+                default:
+                    return "Data incorreta!";
+            }
         }
 
+        if(isset($_GET["data"])){
+            $data = date_parse($_GET["data"]);
+            $signo = getSigno($data["month"], $data["day"]);
+        }
     ?>
-
 </body>
 </html>
